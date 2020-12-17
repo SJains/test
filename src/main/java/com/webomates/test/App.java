050d9762ff3721dc5a7d509b986234b77150f79c
Suhani <Suhani@DESKTOP-LML776L>
2020-12-17 10:45:34 +0530

    updated loggers

--- src/main/java/com/webomates/test/App.java
+++ src/main/java/com/webomates/test/App.java
@@ -17,34 +17,49 @@ import org.json.JSONObject;
  */
 public class App 
 {
-	private static String cycleId = null;
+	private static String cycleId;
 	
-    public static void main( String[] args )
+    public static void main( String[] args ) throws InterruptedException
     {
     	String requestUrl = "https://cq.webomates.com/ci-cd/v1/cycle/";
         String username = "suhani";
         String password = "suhanij@1n";
-        final String POST_PARAMS = "{\r\n" +
-                "\"productId\": 29,\r\n" +
-                "\"scope\": {\r\n" +
-                "\"suiteName\": \"Mini\",\r\n" +
-                "\"suiteType\": \"MINI\"\r\n" +
-                "},\r\n" +
-                "\"environment\": \"Prod\",\r\n" +
-                "\"executionFocus\": \"ACCURACY\",\r\n" +
-                "\"browserMode\": \"HEADLESS_EXECUTION\"\r\n" +
-                "}";
+        String productId = "29";
+    	String suiteName = "Mini";
+    	String suiteType = "MINI";
+    	String environment = "Prod";
+    	String executionFocus = "ACCURACY";
+        final String POST_PARAMS = "{\r\n" + "\"productId\":" + productId + ",\r\n" + "\"scope\": {\r\n" +
+                "\"suiteName\": \"" + suiteName + "\",\r\n" + "\"suiteType\": \"" + suiteType + "\"\r\n" +
+                "},\r\n" + "\"environment\": \"" + environment + "\",\r\n" + "\"executionFocus\": \"" + executionFocus + "\",\r\n" +
+                "\"browserMode\": \"HEADLESS_EXECUTION\"\r\n}";
         try {
-			//postCall(requestUrl, POST_PARAMS, username, password);
-			getCall(requestUrl + "2025662"+ "/results", username, password);
+			String cycleID = postCall(requestUrl, POST_PARAMS, username, password);
+			System.out.println(requestUrl + cycleID + "/status");
+			while(getCall(requestUrl + cycleID + "/status", username, password).getString("cycleStatus").equals("IN_PROGRESS")) {
+				Thread.sleep(60000);
+				System.out.println("Waiting for 60 seconds");
+				JSONObject json = getCall(requestUrl + cycleID + "/status", username, password);
+				JSONObject completion = json.getJSONObject("completionPercentage");
+				System.out.println(json.getLong("cycleId") + " Cycle is In-Progress");
+				System.out.println("Cycle is " + completion.getLong("execute") + "% completed");
+			}
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+			System.out.println(e.getMessage());
 		}
     }
 
-    public static void postCall(String requestUrl,String POST_PARAMS,String username,String password) throws IOException {
-        System.out.println("Request Body >> " +  POST_PARAMS);
+    /**
+     * To hit the POST call
+     * @param requestUrl
+     * @param POST_PARAMS
+     * @param username
+     * @param password
+     * @return
+     * @throws IOException
+     */
+    public static String postCall(String requestUrl,String POST_PARAMS,String username,String password) throws IOException {
+        System.out.println("Request for CI/CD is: " +  POST_PARAMS);
         URL obj = new URL(requestUrl);
         HttpURLConnection postConnection = (HttpURLConnection) obj.openConnection();
         postConnection.setRequestMethod("POST");
@@ -58,8 +73,7 @@ public class App
         os.flush();
         os.close();
         int responseCode = postConnection.getResponseCode();
-        System.out.println("POST Response Code >> " + responseCode);
-        System.out.println("POST Response Message >> " + postConnection.getResponseMessage());
+        System.out.println("Status for cycle request is: " + responseCode);
         if (responseCode == HttpURLConnection.HTTP_CREATED) { // If success
             BufferedReader in = new BufferedReader(new InputStreamReader(
                     postConnection.getInputStream()));
@@ -68,17 +82,30 @@ public class App
             while ((inputLine = in .readLine()) != null) {
                 response.append(inputLine);
             } in .close();
-            System.out.println("POST Response Body >> " + response.toString());
-        } else {                                        // If Failed
+            System.out.println("Response for cycle request is: " + response.toString());
+            JSONObject json = new JSONObject(response.toString());
+            System.out.println("Cycle is successfully created with cycle id: " + json.getLong("cycleId"));
+            cycleId = String.valueOf(json.getLong("cycleId"));
+        } else {
             System.out.println("POST API Request Not Working");
             System.out.println("POST Response Code >> " + responseCode);
             System.out.println("POST Response Message >> " + postConnection.getResponseMessage());
         }
+        return cycleId;
     }
     
-    public static void getCall(String requestUrl, String username,String password) throws IOException {
+    /**
+     * To hit GET API call
+     * @param requestUrl
+     * @param username
+     * @param password
+     * @return
+     * @throws IOException
+     * @throws InterruptedException
+     */
+    public static JSONObject getCall(String requestUrl, String username,String password) throws IOException, InterruptedException {
         URL obj = new URL(requestUrl);
-        System.out.println(requestUrl);
+        JSONObject json = null;
         HttpURLConnection getConnection = (HttpURLConnection) obj.openConnection();
         getConnection.setRequestMethod("GET");
         getConnection.setRequestProperty("Accept", "application/json");
@@ -87,9 +114,7 @@ public class App
         getConnection.setRequestProperty("Authorization", "Basic "+encoded);
         getConnection.setDoOutput(true);
         int responseCode = getConnection.getResponseCode();
-        System.out.println("POST Response Code >> " + responseCode);
-        System.out.println("POST Response Message >> " + getConnection.getResponseMessage());
-        if (responseCode == HttpURLConnection.HTTP_OK) { // If success
+        if (responseCode == HttpURLConnection.HTTP_OK) {
             BufferedReader in = new BufferedReader(new InputStreamReader(
                     getConnection.getInputStream()));
             String inputLine;
@@ -97,13 +122,12 @@ public class App
             while ((inputLine = in .readLine()) != null) {
                 response.append(inputLine);
             } in .close();
-            System.out.println("POST Response Body >> " + response.toString());
-            JSONObject json = new JSONObject(response.toString());
-            System.out.println(json.get("cycleStatus"));
+            json = new JSONObject(response.toString());
         } else {                                        // If Failed
             System.out.println("POST API Request Not Working");
             System.out.println("POST Response Code >> " + responseCode);
             System.out.println("POST Response Message >> " + getConnection.getResponseMessage());
         }
+        return json;
     }
 }
